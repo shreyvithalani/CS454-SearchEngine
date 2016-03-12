5563322ab7ec9211ef9cc8e37bea46271bf10860
Shrey <shrey_vithalani@yahoo.com>
2016-03-11 23:27:16 -0800

    IndexingwithLucene

---
+++ src/main/java/csula/index/Index.java
@@ -0,0 +1,84 @@
+package csula.index;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Index {
+	
+	
+	static StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_35,StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+	
+	public static String indexdir = "indexdir";
+	
+	public static String dir = "/Users/shreyvithalani/Desktop/en";
+	
+	//Directory indexDir = FSDirectory.open(new File(indexdir));
+	
+	IndexWriterConfig indexconfig = new IndexWriterConfig(Version.LUCENE_35,analyzer);	
+	
+	public static void contents(File dir,IndexWriter writer) {
+		try {
+			File[] files = dir.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					//System.out.println("directory:" + file.getCanonicalPath());
+					contents(file,writer);
+				} else {
+					//System.out.println("file:" + file.getCanonicalPath());
+					createIndex(file,writer);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void createIndex(File file,IndexWriter writer) throws IOException{
+		String path = file.getCanonicalPath();
+		Document doc = new Document();
+		doc.add(new Field("path",path,Field.Store.YES,Field.Index.ANALYZED));
+		
+		Reader reader = new FileReader(file);
+		
+		doc.add(new Field("contents",reader,Field.TermVector.YES));
+		
+		writer.addDocument(doc);
+		
+		
+		
+		//IndexWriter index = new IndexWriter();
+		
+		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		//File currentDir = new File("/Users/shreyvithalani/Desktop/en"); // current directory
+		//contents(currentDir);
+		Directory indexdir = FSDirectory.open(new File("indexdir"));
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_35, analyzer);
+		
+		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+		
+		IndexWriter indexwriter = new IndexWriter(indexdir,config);
+		
+		contents(new File(dir),indexwriter);
+		
+		indexwriter.close();
+		
+		
+	}
+
+}
---
+++ src/main/java/csula/index/Searcher.java
@@ -0,0 +1,63 @@
+package csula.index;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Searcher {
+	
+	public static final String INDEX_DIRECTORY = "indexdir";
+
+	public static void main(String[] args) throws IOException, ParseException {
+		// TODO Auto-generated method stub
+		searchIndex("wiki AND zone");
+
+	}
+
+	public static void searchIndex(String searchtext) throws IOException,
+			ParseException {
+		Directory directory = FSDirectory.open(new File(INDEX_DIRECTORY));
+		IndexReader indexReader = IndexReader.open(directory);
+		IndexSearcher searcher = new IndexSearcher(indexReader);
+
+		Analyzer stdAn = new StandardAnalyzer(Version.LUCENE_35);
+		QueryParser parser = new QueryParser(Version.LUCENE_35, "contents",
+				stdAn);
+		Query q = parser.parse(searchtext);
+
+		TopDocs hits = searcher.search(q, 4);
+		ScoreDoc[] scoreDocs = hits.scoreDocs;
+
+		System.out.println("hits = " + scoreDocs.length);
+		System.out.println("Hits (score,  docId)");
+
+		for (int n = 0; n < scoreDocs.length; n++) {
+			ScoreDoc sd = scoreDocs[n];
+			float score = sd.score;
+			int docId = sd.doc;
+
+			System.out.printf("%4.2f  %d\n", score, docId);
+
+			Document document = indexReader.document(docId);
+			String docname = document.getField("path").stringValue();
+			System.out.println(docname);
+		}
+
+		indexReader.close();
+		searcher.close();
+
+	}
+}
